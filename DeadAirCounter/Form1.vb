c7e2381bd0155f1152476cfb53513ade65c2a3b0
captawsm <j.thomas.roberson@gmail.com>
2015-03-11 17:11:40 -0500

    i hope this works....

---
+++ DeadAirCounter/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Label1.Text = 10 Then
+            Label1.Text = 1
+        Else
+            Label1.Text = Label1.Text + 1
+        End If
+    End Sub
+End Class
